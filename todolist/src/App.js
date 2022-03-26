35cfaf6e24a31b4b129bb1d4303b66d2a9256776
sangdph <sangdph.developer@gmail.com>
2022-03-26 15:22:19 +0700

    buoc-2: da hoan thanh

--- todolist/src/App.js
+++ todolist/src/App.js
@@ -1,54 +1,34 @@
 import React from 'react'
 import './App.css';
-
+import Todolist from './Todolist.js';
 class App extends React.Component {
   constructor(props){
     super(props);
-    this.state = {taskName: '', tasks: []}
+    //this.state = {name: ''}
   }
 
-  myTaskChangeHandler = (event) =>{
-    //console.log(event.target.value);
-    // this.setState({ taskname: event.target.value});
-    // console.log('myTaskChangeHandler', this.state.taskName);
-
-    this.setState({taskName: event.target.value});
-    //console.log('Sang', this.state.taskName);
-  }
-
-  addTask = ()=>{
-    //console.log('addTask', this.state.taskName);
-    if(this.state.taskName === ''){
-      return;
-    }
-    this.state.tasks.push(this.state.taskName);
-    this.setState({taskName: ''});
-  }
   render(){
     return (
       <div className="App">
-          To do List Sin236
-          <br/>
-          <div className='aligned'>
-          <img
-            src='./assets/24212_package_install_icon.png'
-            alt='Add Task'
-            width='25'
-            style={{cursor: 'pointer'}}
-            title='Bấm vào đây để thêm task'
-            onClick={()=>this.addTask()}
-          />
-          <input 
-            type='text'
-            value={this.state.taskName}
-            onChange={this.myTaskChangeHandler}
-          />
-          </div>
-          <ul>
-            {this.state.tasks.map((value, index)=>{
-              return <li key={index}>{value}</li>
-            })}
-          </ul>
+          <table border='1'>
+            <thead>
+              <tr>
+                <th>Nhiệm vụ 1</th>
+                <th>Nhiệm vụ 2</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr>
+                <td>
+                  <Todolist name='Đi chợ'/>
+                </td>
+                <td>
+                  <Todolist name='Đi shopping'/>
+                </td>
+              </tr>
+            </tbody>
+          </table>
+          
       </div>
     );
   }
